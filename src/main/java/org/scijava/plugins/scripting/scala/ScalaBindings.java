5a950e60b46898a079d92ea75f95b2b6ee137fac
Curtis Rueden <ctrueden@wisc.edu>
2015-02-11 07:05:42 -0600

    Happy belated New Year 2015

--- src/main/java/org/scijava/plugins/scripting/scala/ScalaBindings.java
+++ src/main/java/org/scijava/plugins/scripting/scala/ScalaBindings.java
@@ -2,7 +2,7 @@
  * #%L
  * JSR-223-compliant Scala scripting language plugin.
  * %%
- * Copyright (C) 2013 - 2014 Board of Regents of the University of
+ * Copyright (C) 2013 - 2015 Board of Regents of the University of
  * Wisconsin-Madison.
  * %%
  * Redistribution and use in source and binary forms, with or without
