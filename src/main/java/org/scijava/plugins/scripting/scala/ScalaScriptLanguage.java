1b7b9e89c52104c0d3f877b648b94d3ea6f047c7
Keith Schulze <keith.schulze@gmail.com>
2016-11-02 09:10:30 +1100

    Update to Scala 2.12.0
    This updates to Scala 2.12.0. Script engine support has been refactored
    since the last scripting-scala release. See this commit:
    https://github.com/scala/scala/commit/3cddeaa525fd6fe9860a27019fdf484297a8d3dd
    
    No longer requires binding of attribute names with type (e.g. name: Type)
    to get typed values. Thus we no longer need to override ScriptEngine
    methods.

--- src/main/java/org/scijava/plugins/scripting/scala/ScalaScriptLanguage.java
+++ src/main/java/org/scijava/plugins/scripting/scala/ScalaScriptLanguage.java
@@ -7,13 +7,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -30,15 +30,28 @@
 
 package org.scijava.plugins.scripting.scala;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.stream.Collectors;
 import javax.script.ScriptEngine;
 
 import org.scijava.plugin.Plugin;
 import org.scijava.script.AdaptedScriptLanguage;
 import org.scijava.script.ScriptLanguage;
 
+import scala.tools.nsc.ConsoleWriter;
+import scala.tools.nsc.NewLinePrintWriter;
+import scala.tools.nsc.Settings;
+import scala.tools.nsc.interpreter.Scripted;
+
 /**
  * An adapter of the Scala interpreter to the SciJava scripting interface.
- * 
+ *
  * @author Curtis Rueden
  * @author Johannes Schindelin
  * @see ScriptEngine
@@ -46,12 +59,51 @@ import org.scijava.script.ScriptLanguage;
 @Plugin(type = ScriptLanguage.class, name = "Scala")
 public class ScalaScriptLanguage extends AdaptedScriptLanguage {
 
-	public ScalaScriptLanguage() {
-		super("scala");
-	}
+    public ScalaScriptLanguage() {
+        super("scala");
+    }
 
-	@Override
-	public ScriptEngine getScriptEngine() {
-		return new ScalaScriptEngine(super.getScriptEngine());
-	}
+    @Override
+    public ScriptEngine getScriptEngine() {
+        // The Scala script engine uses the "java.class.path" property from System
+        // properties to resolve calls to other libraries; however, when
+        // launched imagej-launcher, only the ImageJ-launcher is available in
+        // System.getProperty("java.class.path"). Therefore, we check whether
+        // the java.class.path and classpath according according to ClassLoader
+        // are equal and update accordingly.
+        updateJavaCP();
+
+        Settings settings = new Settings();
+        settings.usemanifestcp().value_$eq(true);
+
+        // Scripted.apply sets settings.usejavacp = true
+        return Scripted.apply(new Scripted.Factory(), settings,
+                new NewLinePrintWriter(new ConsoleWriter(), true));
+    }
+
+    /**
+     * Uses ClassLoader to generate a string of the current classpath separated by OS
+     * specific separator.
+     */
+    private String getClasspath() {
+        return Arrays.stream(((URLClassLoader) ClassLoader.getSystemClassLoader()).getURLs())
+            .map(url -> url.getPath())
+            .collect(Collectors.joining(System.getProperty("path.separator")));
+    }
+
+    /**
+     *  Checks whether "java.class.path" property in System properties and
+     *  classpath according to ClassLoader are equals and updates java.class.path
+     *  if necessary.
+     */
+    private void updateJavaCP() {
+        String cp = getClasspath();
+
+        if (!Objects.equals(System.getProperty("java.class.path"), cp)) {
+            Properties p = new Properties(System.getProperties());
+            p.setProperty("java.class.path", cp);
+
+            System.setProperties(p);
+        }
+    }
 }
